68867e5f6d9d1c59d078e1bbc51c739bddce0816
SanaOsman21 <sanaosman8922@gmail.com>
2025-10-16 21:29:19 +0530

    objects introduction

---
+++ week-1(JS FOUNDATION)/level-2/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let fruit = prompt("Which fruit to buy?");
+
+// let bag = {
+//   [fruit]: 5, // the name of the property is taken from the variable fruit
+// };
+// console.log(bag[fruit]); // shows '5' for whatever fruit the user entered
